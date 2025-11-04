9795fd0390c327a642e9b1dd809db3a3f8f886ed
Tomas Hofman <thofman@redhat.com>
2025-11-04 13:53:50 +0100

    Add API methods that allow to override manifest versions for upgrade ops

---
+++ src/main/java/org/wildfly/installationmanager/InstallationUpdates.java
@@ -0,0 +1,59 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2023 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wildfly.installationmanager;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Represents possible incoming updates applicable to the installation.
+ */
+public class InstallationUpdates {
+
+    private final List<ArtifactChange> artifactUpdates;
+    private final List<ManifestVersionChange> manifestUpdates;
+
+    /**
+     * Constructor
+     * @param artifactUpdates list of artifact changes or empty list if no changes found
+     * @param manifestUpdates list of channel manifest version changes or empty list if no changes found
+     */
+    public InstallationUpdates(List<ArtifactChange> artifactUpdates, List<ManifestVersionChange> manifestUpdates) {
+        Objects.requireNonNull(artifactUpdates);
+        Objects.requireNonNull(manifestUpdates);
+
+        this.artifactUpdates = Collections.unmodifiableList(artifactUpdates);
+        this.manifestUpdates = Collections.unmodifiableList(manifestUpdates);
+    }
+
+    /**
+     * @return list of artifact changes or empty list if no changes found
+     */
+    public List<ArtifactChange> artifactUpdates() {
+        return artifactUpdates;
+    }
+
+    /**
+     * @return list of channel manifest version changes or empty list if no changes found
+     */
+    public List<ManifestVersionChange> manifestUpdates() {
+        return manifestUpdates;
+    }
+}
